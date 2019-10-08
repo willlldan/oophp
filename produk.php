6a7d3599c170083b0c60a4ac6eaf7d5cb368d1dd
willlldan <173040071@mail.unpas.ac.id>
2019-10-08 10:58:01 +0700

    Video Ketiga - Property dan Method

--- produk.php
+++ produk.php
@@ -1,8 +1,37 @@
 <?php 
 
 class Produk {
+   public $judul = "Judul", 
+            $penulis = "Penulis", 
+            $penerbit = "Penerbit",
+            $harga = "Harga"; 
 
+    public function getLabel() {
+        return "$this->penulis, $this->penerbit";
+    }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+// echo "<hr>";
+
+// $produk2 = new Produk();
+// $produk2->judul = "Crach Bandicot";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckman";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();

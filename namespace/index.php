31527f5f3f4453bc4d791520e1eb8b573ed99183
willlldan <173040071@mail.unpas.ac.id>
2019-10-09 07:35:41 +0700

    Video Ketujuhbelas - Namespace (Problem ketika ada nama kelas yang sama pada folder yang berbeda)

---
+++ namespace/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+require_once 'App/init.php';
+
+// $produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+// $produk2 = new Game("Uncharted", "Neil Druckman", "Sony Computer", 250000, 55);
+
+// $cetakProduk = new CetakInfoProduk();
+// $cetakProduk->tambahProduk($produk1);
+// $cetakProduk->tambahProduk($produk2);
+// echo $cetakProduk->cetakProduk();
+use App\Service\User as ServiceUser;
+use App\Produk\User as ProdukUser;
+new ServiceUser();
+echo "<br>";
+new ProdukUser();
